a9fafffa0918f6252a1ba23ddb25a67ed1b16acf
animan alloua yannhervee <animan001@gannon.edu>
2022-03-06 17:04:27 -0500

    updated navbar

--- components/Nav.js
+++ components/Nav.js
@@ -3,6 +3,9 @@ import Link from 'next/link'
 import navStyles from '../styles/Nav.module.css'
 import logo from '../public/wtransparent.png' 
 import Image from 'next/image'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import{ faBars, faRocket, faBell} from "@fortawesome/free-solid-svg-icons";
+
 
 const Nav = () => {
   return (
@@ -14,15 +17,22 @@ const Nav = () => {
             />
         </div>
         <div>
-          <a>Student connection </a>
+          <a className={navStyles.logotilte}>Student connection </a>
         </div>
         <div className={navStyles.right}>
             <ul>
                 <li>
-                <Link href='/'>Home</Link>
+                  <Link href='/myquestions'><button className={navStyles.postquestionbutton}>Post a question</button></Link>
+                  
                 </li>
                 <li>
-                <Link href='/about'>About</Link>
+                <Link href='/'><FontAwesomeIcon icon={faBell} style={{width:"18px", cursor:"pointer"}}/></Link>
+                </li>
+                <li>
+                <Link href='/about'> Beautiful</Link>
+                </li>
+                <li>
+                <Link href='/dashboard'><FontAwesomeIcon icon={faBars} style={{width:"18px", cursor:"pointer"}}/></Link>
                 </li>
             </ul>
         </div>
