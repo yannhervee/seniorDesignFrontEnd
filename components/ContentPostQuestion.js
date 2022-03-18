736f6a3fae05a7f7b5eb9a433956cc64854d05c7
animan alloua yannhervee <animan001@gannon.edu>
2022-03-18 12:31:55 -0400

    changed form to react final form

--- components/ContentPostQuestion.js
+++ components/ContentPostQuestion.js
@@ -1,10 +1,25 @@
 import styles from '../styles/ContentPostQuestion.module.css'
 import React from 'react'
 import Link from 'next/link'
+import {Form, Field} from 'react-final-form'
 
 
 const ContentPostQuestion = () => {
  
+    const required = value => (value ? undefined : 'Required')
+   
+    const composeValidators = (...validators) => value =>
+    validators.reduce((error, validator) => error || validator(value), undefined)
+
+
+    const onSubmit = async event => {
+        event.preventDefault()
+
+
+    }
+
+
+
 
     return (
         <>
@@ -31,47 +46,84 @@ const ContentPostQuestion = () => {
             
 			
 			<div className={styles.coursestab}> 
-                <form>
-                    <div className={styles.stepwrapper}>
-                        <h3 className={styles.label}> 1- Tell us what course you need help with </h3>
-                    </div>
-                    
-                    <div className={styles.coursewrapper}>
-                        <select id="course" 
-                            className={styles.selectinput} 
-                            //onChange={e => setRole(e.target.value)}
-                            required
+                 <Form
+                    onSubmit={onSubmit} 
+                    initialValues={{ course: '', topic :''}}
+                    render={({handleSubmit, form, submitting, pristine, values }) => (
+                        <form onSubmit={handleSubmit}>
+    `                        <div className={styles.stepwrapper}>
+                                <label className={styles.label}> 1- Tell us what course you need help with * </label>
+                            </div>
+                            
+                            <div className={styles.coursewrapper}>
+                                <Field name="course" 
+                                    className={styles.selectinput} 
+                                    //onChange={e => setRole(e.target.value)}
+                                    component="select"
+                                    validate={required}
+                                    
+                                    >
+                                    <option value="" disabled selected>Select a course </option>
+                                    <option value="#">MATlabel10</option>
+                                    <option value="#">SOFT210</option>    
+                                </Field>
+                                
+                            </div>
+
+                            <div className={styles.topicwrapper}>
+                                <label className={styles.label}> 2- Topic you need help with * </label>
+                            </div>
+                            
+                            
+                                    <Field 
+                                    type="text" 
+                                    name="topic" 
+                                    
+                                    component="input"
+                                    validate={required}
+                                    >
+                                        {({ input, meta }) => (
+                                            <div>
+                                                
+                                                <input {...input} type="text" className={styles.topicinput} />
+                                                {meta.error && meta.touched && <span>{meta.error}</span>}
+                                            </div>
+                                        )} 
+                                    </Field>
+                            
+                            <div className={styles.questionwrapper}>
+                                <label className={styles.label}> 3- What is your question? * </label>
+                            </div>
+                            
+                            
+                            <Field 
+                                type="text" 
+                                name="question" 
+                                className={styles.questioninput} 
+                                component="input" 
+                                validate={required} 
                             >
-                            <option value="" disabled selected>Select a course</option>
-                            <option value="#">MATH310</option>
-                            <option value="#">SOFT210</option>    
-                        </select>
-                        
-                    </div>
+                                 
+                                        {({ input, meta }) => (
+                                            <div>
+                                                
+                                                <input {...input} type="text"className={styles.questioninput} />
+                                                {meta.error && meta.touched && <span>{meta.error}</span>}
+                                            </div>
+                                        )} 
+                                    </Field>
 
-                    <div className={styles.topicwrapper}>
-                        <h3 className={styles.label}> 2- Topic you need help with </h3>
-                    </div>
+                          
+                                            
+                            <div>
+                                <button type="submit" className={styles.button}>Submit</button>
+                            </div>
+                            
+                    </form>
+                    )}
                     
-                            <input type="text" id="topic" className={styles.topicinput} required />
-                    
-
-                    <div className={styles.questionwrapper}>
-                        <h3 className={styles.label}> 3- What is your question? </h3>
-                    </div>
-                    
-                    
-                    <input type="text" id="question" className={styles.questioninput} required />
-                    
-
-                   
-                
-                    <div>
-                        <button type="submit" className={styles.button}>Submit</button>
-                    </div>
-                </form>
-                
-				    
+                />
+				     
 
 			</div>
             </>
