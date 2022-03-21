207de0c80edee1294a49822072f4d58d0ddd1b25
animan alloua yannhervee <animan001@gannon.edu>
2022-03-21 11:00:10 -0400

    forum

--- pages/forum.js
+++ pages/forum.js
@@ -2,81 +2,74 @@ import LeftNav from '../components/LeftNav';
 import Header from '../components/Header';
 import ContentDashboard from '../components/ContentDashboard';
 import styles from '../styles/MyQuestions.module.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-const Myquestions = () => {
+const Forum = () => {
     const [posts, setPosts] = useState([])
+    useEffect(() => {
+        console.log('running effect')
+      //  console.log(user, 'department')
+        // Promises
+        axios.get('http://localhost:3001/posts/', {
+           
+        })
+        .then((res) => {
+            console.log(res);  
+            setPosts(res.data)   
+            console.log("res data posts all", posts)   
+            console.log("res data posts", posts[0])
+
+        })
+        .catch((e) => {
+            console.log('error', e);
+        })
+    }, [])
+
     
     return(
         <>
-
-
-            
 
         <div></div>
         
         <LeftNav />
         <div className={styles.container}>
             <h1 className={styles.title}> Forum </h1>
-            <div className={styles.question}>
+
+       
+            
+               
+
+                {posts.map((post) => {
+                    console.log("single post", post)
+                    console.log("single post body", post.body)
+                    return   <>
+                    <div className={styles.question}>
                 <div className={styles.userinfo}>
                     <div className={styles.circle}>B</div>
                 </div>
-                <div className={styles.questioninfo}>
-                    <span className={styles.questioncontent}>I need help with finding derivating of logarithmic and exponential function. </span>
-                    <span className={styles.topic}> Derivative</span>
+                                <div className={styles.questioninfo}>
+                                    <span className={styles.questioncontent}>{post.body}</span>
+                                    <span className={styles.topic}> {post.topic.name}</span>
+                                </div>
+                                <div className={styles.comments}> 5 comments </div>
+                                <div className={styles.course}>{post.topic.course_id}</div>
+                                </div>
+                            </>
+                        
 
-                </div>
-                <div className={styles.comments}> 5 comments </div>
-                <div className={styles.course}>Math 310</div>
-
-            </div>
-
-            <div className={styles.question}>
-                <div className={styles.userinfo}>
-                    <div className={styles.circle}>B</div>
-                </div>
-                <div className={styles.questioninfo}>
-                    <span className={styles.questioncontent}>I need help with finding derivating of logarithmic and exponential function. </span>
-                    <span className={styles.topic}> Derivative</span>
-
-                </div>
-                <div className={styles.comments}> 5 comments </div>
-                <div className={styles.course}>Math 310</div>
-
-            </div>
-            <div className={styles.question}>
-                <div className={styles.userinfo}>
-                    <div className={styles.circle}>B</div>
-                </div>
-                <div className={styles.questioninfo}>
-                    <span className={styles.questioncontent}>I need help with finding derivating of logarithmic and exponential function. </span>
-                    <span className={styles.topic}> Antiderivative</span>
-
-                </div>
-                <div className={styles.comments}> 5 comments </div>
-                <div className={styles.course}>MATH 310</div>
-
-            </div>
-            <div className={styles.question}>
-                <div className={styles.userinfo}>
-                    <div className={styles.circle}>B</div>
-                </div>
-                <div className={styles.questioninfo}>
-                    <span className={styles.questioncontent}>I need help with finding derivating of logarithmic and exponential function. </span>
-                    <span className={styles.topic}> Gravity</span>
-
-                </div>
-                <div className={styles.comments}> 0 comments </div>
-                <div className={styles.course}>Physics</div>
+                      
+                })}
+                     
 
             </div>
            
 
-        </div>
+        
         
    
         </>
     )
 }
 
-export default Myquestions;
+export default Forum;
